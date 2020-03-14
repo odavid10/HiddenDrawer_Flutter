eaaaffa6cc98782a81c3d25e7115c2281a0de7b1
Omar David <oder1011119504@gmail.com>
2020-03-14 14:53:01 -0400

    Aplicacion terminada

---
+++ lib/src/pages/home.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:hidden_drawer_menu/hidden_drawer/hidden_drawer_menu.dart';
+
+void main() => runApp(MyApp());
+
+class HomePage extends StatefulWidget {
+  HomePage({Key key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  List<ScreenHiddenDrawer> itens = new List();
+
+  @override
+  void initState() {
+    itens.add(new ScreenHiddenDrawer(
+        new ItemHiddenMenu(
+          name: "Página 1",
+          baseStyle: TextStyle( color: Colors.white.withOpacity(0.8), fontSize: 28.0 ),
+          colorLineSelected: Colors.teal,
+        ),
+        FirstPage()
+      )
+    );
+
+    itens.add(new ScreenHiddenDrawer(
+        new ItemHiddenMenu(
+          name: "Página 2",
+          baseStyle: TextStyle( color: Colors.white.withOpacity(0.8), fontSize: 28.0 ),
+          colorLineSelected: Colors.orange,
+        ),
+        SecondPage()
+      )
+    );
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return HiddenDrawerMenu(
+      backgroundColorMenu: Colors.blueGrey,
+      backgroundColorAppBar: Colors.cyan,
+      screens: itens,
+        //    typeOpen: TypeOpen.FROM_RIGHT,
+        //    enableScaleAnimin: true,
+        //    enableCornerAnimin: true,
+        slidePercent: 60.0,
+        verticalScalePercent: 80.0,
+        contentCornerRadius: 40.0,
+        //    iconMenuAppBar: Icon(Icons.menu),
+        //    backgroundContent: DecorationImage((image: ExactAssetImage('assets/bg_news.jpg'),fit: BoxFit.cover),
+        //    whithAutoTittleName: true,
+        //    styleAutoTittleName: TextStyle(color: Colors.red),
+        //    actionsAppBar: <Widget>[],
+        //    backgroundColorContent: Colors.blue,
+        //    elevationAppBar: 4.0,
+        //    tittleAppBar: Center(child: Icon(Icons.ac_unit),),
+        //    enableShadowItensMenu: true,
+        backgroundMenu: DecorationImage(
+          image: NetworkImage('https://fsb.zobj.net/crop.php?r=iLyhXwU27oCz7PXJKSoD3FOc0LVx7iAuqoW2lknHKQ5ymmhS3q5FZOqz-1S6vjwB7t06C-4_Q3K_akt4jm_NlEL_8pDusC9a1Fs9s7oCtcPRKmEKVyFMW32LbwnVb75qu0RWi_y0cNsGPbW-'),
+          fit: BoxFit.cover
+        ),
+    );
+  }
+}
